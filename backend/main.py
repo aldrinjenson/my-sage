a0781cd0b7b4ae3f62ec1dae1cf0b49cba02c239
Aldrin Jenson <aldrinjenson@gmail.com>
2023-05-19 18:49:02 +0530

    add backend

---
+++ backend/main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+# from ingest import ingest_docs
+
+app = FastAPI()
+
+# CORS configuration
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    # Add more allowed origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/status")
+async def get_status():
+    return {"message": "Server active"}
+
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    file_path = f"src/{file.filename}"
+    with open(file_path, "wb") as f:
+        f.write(contents)
+    return {"filename": file.filename, "status": "uploaded"}
+
+
+@app.post("/train")
+async def train_model(filename: str, urls: list, userId: str):
+    print(filename, urls, userId)
+    # response = await ingest_docs(filename, urls, userId)
+    return {"response": "vector store created"}
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
